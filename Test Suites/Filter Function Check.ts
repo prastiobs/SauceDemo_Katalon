510b1ff4012896d28988e3ed3c711ed05e7f2f82
ISGS - Prastio Bagusindra <isgs.prastio@bankmandiri.co.id>
2024-08-29 00:13:34 +0700

    update test data

--- Test Suites/Filter Function Check.ts
+++ Test Suites/Filter Function Check.ts
@@ -16,17 +16,26 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC_002_User success login using valid credentials</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>27c29a21-2275-4b16-a54b-22eff3c65f21</id>
+         <iterationEntity>
+            <iterationType>SPECIFIC</iterationType>
+            <value>1</value>
+         </iterationEntity>
+         <testDataId>Data Files/Login</testDataId>
+      </testDataLink>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
+         <testDataLinkId>27c29a21-2275-4b16-a54b-22eff3c65f21</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
          <variableId>16bb066e-b302-4905-9869-6322286e4abf</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
+         <testDataLinkId>27c29a21-2275-4b16-a54b-22eff3c65f21</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
          <variableId>4cb9272a-9483-448f-bf49-afd151484c82</variableId>
       </variableLink>
    </testCaseLink>
